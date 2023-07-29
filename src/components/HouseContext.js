e0cf09343d717942fdb99c672f1c11fc434ba9aa
Tanvir Ahamed Sowmik <111014007+sowmik265@users.noreply.github.com>
2023-07-30 02:49:01 +0600

    dropdown working

--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import { housesData } from '../data';
 
-const HouseContext = () => {
-  return <div>HouseContext</div>;
+export const HouseContext = createContext();
+
+const HouseContextProvider = ({ children }) => {
+  const [houses, setHouses] = useState(housesData);
+  const [country, setCountry] = useState('Location (any)');
+  const [countries, setCountries] = useState([]);
+  const [property, setProperty] = useState('Property type (any)');
+  const [properties, setProperties] = useState([]);
+  const [price, setPrice] = useState('Price range (any)');
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const allCountries = houses.map(house => {
+      return house.country;
+    });
+
+    const uniqueCountries = ['Location (any)', ...new Set(allCountries)];
+    setCountries(uniqueCountries);
+  }, []);
+
+  useEffect(() => {
+    const allProperties = houses.map(house => {
+      return house.type;
+    });
+
+    const uniqueProperties = ['Location (any)', ...new Set(allProperties)];
+    setProperties(uniqueProperties);
+  }, []);
+
+  return (
+    <HouseContext.Provider value={{ country, setCountry, countries, property, setProperty, properties, price, setPrice, houses, loading }}>
+      {children}
+    </HouseContext.Provider>
+
+  );
 };
 
-export default HouseContext;
+export default HouseContextProvider;
--- src/components/PriceRangeDropdown.js
+++ src/components/PriceRangeDropdown.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { RiWallet3Line, RiArrowDownSLine, RiArrowUpSLine } from 'react-icons/ri';
+import { Menu } from '@headlessui/react'
+import { HouseContext } from './HouseContext';
 
 const PriceRangeDropdown = () => {
-  return <div>PriceRangeDropdown</div>;
+  const { price , setPrice } = useContext(HouseContext);
+  const [isOpen, setIsOpen] = useState(false);
+  return <Menu as='div' className='dropdown relative'>
+    <Menu.Button onClick={() => setIsOpen(!isOpen)} className='dropdown-btn w-full text-left'>
+      <RiWallet3Line className='dropdown-icon-primary'></RiWallet3Line>
+      <div>
+        <div className='text-[15px] font-medium leading-tight'>{property}</div>
+        <div className='text-[13px]'>Choose price range</div>
+      </div>
+      {
+        isOpen ? (<RiArrowDownSLine className='dropdown-icon-secondary'></RiArrowDownSLine>) : (<RiArrowUpSLine className='dropdown-icon-secondary'></RiArrowUpSLine>)
+      }
+    </Menu.Button>
+    <Menu.Items className='dropdown-menu'>
+      {properties.map((property, index) => {
+        return (<Menu.Item onClick={() => setProperty(property)} className='cursor-pointer hover:text-violet-700 transition' as='li' key={index}>
+          {property}
+        </Menu.Item>)
+      })}
+    </Menu.Items>
+  </Menu>;
 };
 
 export default PriceRangeDropdown;
